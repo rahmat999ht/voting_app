9ca02dafc0ce5a0d6f8d40436c738863a88ae53d
Ming <muhmuhaimin15@gmail.com>
2023-07-10 19:16:20 +0800

    comit pertama

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:packages/extensions/size_app.dart';
+import 'package:packages/forms/form.dart';
 
 import '../controllers/home_controller.dart';
 
@@ -9,16 +11,32 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('Voting App'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: ListView(
+          padding: const EdgeInsets.all(20),
+          children: [
+            Center(
+              child: const Text(
+                "Enter A Vote Code",
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+            20.sH,
+            TextField(
+              autocorrect: false,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: "Entar code",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            10.sH,
+            ElevatedButton(onPressed: () {}, child: Text("Validate")),
+            10.sH,
+          ],
+        ));
   }
 }
