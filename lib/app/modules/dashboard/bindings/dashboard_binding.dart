6aa4ab70b4cad7d73006f63c95042807bba7a63f
yayat_99 <rahmat999ht@gmail.com>
2023-07-10 19:16:58 +0800

    update screen hasil

--- lib/app/modules/dashboard/bindings/dashboard_binding.dart
+++ lib/app/modules/dashboard/bindings/dashboard_binding.dart
@@ -1,12 +1,29 @@
 import 'package:get/get.dart';
+import 'package:voting_app/app/modules/hasil/controllers/hasil_controller.dart';
+import 'package:voting_app/app/modules/home/controllers/home_controller.dart';
+import 'package:voting_app/app/modules/pemilihan/controllers/pemilihan_controller.dart';
+import 'package:voting_app/app/modules/profile/controllers/profile_controller.dart';
 
 import '../controllers/dashboard_controller.dart';
 
 class DashboardBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<DashboardController>(
-      () => DashboardController(),
+    Get.put<DashboardController>(
+      DashboardController(),
+      permanent: true,
+    );
+    Get.lazyPut<HomeController>(
+      () => HomeController(),
+    );
+    Get.lazyPut<PemilihanController>(
+      () => PemilihanController(),
+    );
+    Get.lazyPut<HasilController>(
+      () => HasilController(),
+    );
+    Get.lazyPut<ProfileController>(
+      () => ProfileController(),
     );
   }
 }
--- lib/app/modules/hasil/controllers/hasil_controller.dart
+++ lib/app/modules/hasil/controllers/hasil_controller.dart
@@ -1,23 +1,5 @@
 import 'package:get/get.dart';
 
 class HasilController extends GetxController {
-  //TODO: Implement HasilController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  final valueLiquid = 0.76.obs;
 }
