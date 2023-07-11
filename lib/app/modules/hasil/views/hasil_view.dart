eb26e44ff9544a4d0ff251c6d8a1346c67d31d15
Ming <muhmuhaimin15@gmail.com>
2023-07-11 21:24:30 +0800

    update pemilihan, profil, hasil final

--- lib/app/modules/hasil/views/hasil_view.dart
+++ lib/app/modules/hasil/views/hasil_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_echarts/flutter_echarts.dart';
 import 'package:get/get.dart';
+import 'package:packages/extensions/size_app.dart';
 import 'package:voting_app/app/modules/hasil/components/liquid_script.dart';
 
 import '../controllers/hasil_controller.dart';
@@ -14,31 +15,102 @@ class HasilView extends GetView<HasilController> {
         title: const Text('HasilView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text('Progress...'),
-            SizedBox(
-              width: 400,
-              height: 400,
-              child: Obx(
-                () => Echarts(
-                  extensions: const [liquidScript],
-                  // theme: 'dark',
-                  option: '''
-                                  {
-                                    series: [{
-                                        type: 'liquidFill',
-                                        data: [${controller.valueLiquid.value}]
-                                    }]
-                                  }
-                                ''',
+      body: HasilAkhir(controller: controller),
+    );
+  }
+}
+
+class HasilAkhir extends StatelessWidget {
+  const HasilAkhir({super.key, required this.controller});
+  final HasilController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.grey[300],
+        borderRadius: BorderRadius.circular(20),
+        child: InkWell(
+          onTap: () {
+            print("object");
+          },
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            //height: 130,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                12.sH,
+                Text(
+                  "Selamat kepada yang terpilih",
+                  style: TextStyle(fontSize: 24),
                 ),
+                12.sH,
+                Text(
+                  "No 1, Nama",
+                  style: TextStyle(fontSize: 20),
+                ),
+                12.sH,
+                ClipOval(
+                  child: Container(
+                    height: 160,
+                    width: 160,
+                    color: Colors.grey[200],
+                    child: Image.network(
+                      "https://ui-avatars.com/api/?name=John+Doe",
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                12.sH,
+                Text(
+                  "Dengan total suara 90% ",
+                  style: TextStyle(fontSize: 20),
+                ),
+                12.sH,
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HasilSementara extends StatelessWidget {
+  const HasilSementara({
+    super.key,
+    required this.controller,
+  });
+
+  final HasilController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Text('Progress...'),
+          SizedBox(
+            width: 400,
+            height: 400,
+            child: Obx(
+              () => Echarts(
+                extensions: const [liquidScript],
+                // theme: 'dark',
+                option: '''
+                                {
+                                  series: [{
+                                      type: 'liquidFill',
+                                      data: [${controller.valueLiquid.value}]
+                                  }]
+                                }
+                              ''',
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/app/modules/pemilihan/views/pemilihan_view.dart
+++ lib/app/modules/pemilihan/views/pemilihan_view.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:packages/extensions/size_app.dart';
 
 import '../components/indicator.dart';
 import '../controllers/pemilihan_controller.dart';
@@ -15,88 +16,164 @@ class PemilihanView extends GetView<PemilihanController> {
         title: const Text('PemilihanView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: AspectRatio(
-          aspectRatio: 1.3,
-          child: Row(
-            children: <Widget>[
-              const SizedBox(
-                height: 18,
-              ),
-              Expanded(
-                child: AspectRatio(
-                  aspectRatio: 1,
-                  child: Obx(
-                    () => PieChart(
-                      PieChartData(
-                        pieTouchData: PieTouchData(
-                          touchCallback:
-                              (FlTouchEvent event, pieTouchResponse) {
-                            if (!event.isInterestedForInteractions ||
-                                pieTouchResponse == null ||
-                                pieTouchResponse.touchedSection == null) {
-                              controller.touchedIndex.value = -1;
-                              return;
-                            }
-                            controller.touchedIndex.value = pieTouchResponse
-                                .touchedSection!.touchedSectionIndex;
-                          },
-                        ),
-                        borderData: FlBorderData(
-                          show: false,
-                        ),
-                        sectionsSpace: 0,
-                        centerSpaceRadius: 40,
-                        sections: showingSections(),
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            padding: EdgeInsets.all(20),
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                Material(
+                  color: Colors.grey[400],
+                  borderRadius: BorderRadius.circular(20),
+                  child: InkWell(
+                    onTap: () {
+                      print("object");
+                    },
+                    child: SizedBox(
+                      width: 130,
+                      //height: 130,
+                      child: Column(
+                        children: [
+                          12.sH,
+                          Text(
+                            "No 1, Nama",
+                            style: TextStyle(fontSize: 16),
+                          ),
+                          5.sH,
+                          ClipOval(
+                            child: Container(
+                              color: Colors.grey[200],
+                              child: Image.network(
+                                "https://ui-avatars.com/api/?name=John+Doe",
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          5.sH,
+                          Card(
+                              color: Colors.yellow,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text("15%"),
+                              )),
+                          12.sH,
+                        ],
                       ),
                     ),
                   ),
                 ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
-                  Indicator(
-                    color: Colors.blue,
-                    text: 'First',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Colors.yellow,
-                    text: 'Second',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Colors.purple,
-                    text: 'Third',
-                    isSquare: true,
-                  ),
-                  SizedBox(
-                    height: 4,
-                  ),
-                  Indicator(
-                    color: Colors.green,
-                    text: 'Fourth',
-                    isSquare: true,
-                  ),
-                  SizedBox(
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data"),
+                20.sW,
+                Text("data")
+              ],
+            ),
+          ),
+          Center(
+            child: AspectRatio(
+              aspectRatio: 1.3,
+              child: Row(
+                children: <Widget>[
+                  const SizedBox(
                     height: 18,
+                  ),
+                  Expanded(
+                    child: AspectRatio(
+                      aspectRatio: 1,
+                      child: Obx(
+                        () => PieChart(
+                          PieChartData(
+                            pieTouchData: PieTouchData(
+                              touchCallback:
+                                  (FlTouchEvent event, pieTouchResponse) {
+                                if (!event.isInterestedForInteractions ||
+                                    pieTouchResponse == null ||
+                                    pieTouchResponse.touchedSection == null) {
+                                  controller.touchedIndex.value = -1;
+                                  return;
+                                }
+                                controller.touchedIndex.value = pieTouchResponse
+                                    .touchedSection!.touchedSectionIndex;
+                              },
+                            ),
+                            borderData: FlBorderData(
+                              show: false,
+                            ),
+                            sectionsSpace: 0,
+                            centerSpaceRadius: 40,
+                            sections: showingSections(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const <Widget>[
+                      Indicator(
+                        color: Colors.blue,
+                        text: 'First',
+                        isSquare: true,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Indicator(
+                        color: Colors.yellow,
+                        text: 'Second',
+                        isSquare: true,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Indicator(
+                        color: Colors.purple,
+                        text: 'Third',
+                        isSquare: true,
+                      ),
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Indicator(
+                        color: Colors.green,
+                        text: 'Fourth',
+                        isSquare: true,
+                      ),
+                      SizedBox(
+                        height: 18,
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 28,
                   ),
                 ],
               ),
-              const SizedBox(
-                width: 28,
-              ),
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
